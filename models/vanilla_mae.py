ae313ce1e596bada717bf883863673be7c8d2f96
bqm1111 <buiquangminh1111@gmail.com>
2024-04-17 14:35:18 +0200

    Using newest timm version

--- models/vanilla_mae.py
+++ models/vanilla_mae.py
@@ -40,8 +40,8 @@ class MaskedAutoencoderViT(nn.Module):
             1, num_patches + 1, embed_dim), requires_grad=False)  # fixed sin-cos embedding
 
         self.blocks = nn.ModuleList([
-            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True,
-                  qk_scale=None, norm_layer=norm_layer)
+            Block(embed_dim, num_heads, mlp_ratio,
+                  qkv_bias=True, norm_layer=norm_layer)
             for i in range(depth)])
         self.norm = norm_layer(embed_dim)
         # --------------------------------------------------------------------------
@@ -57,7 +57,7 @@ class MaskedAutoencoderViT(nn.Module):
 
         self.decoder_blocks = nn.ModuleList([
             Block(decoder_embed_dim, decoder_num_heads, mlp_ratio,
-                  qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+                  qkv_bias=True, norm_layer=norm_layer)
             for i in range(decoder_depth)])
 
         self.decoder_norm = norm_layer(decoder_embed_dim)
